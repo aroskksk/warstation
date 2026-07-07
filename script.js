9d6e540d2d6a7fce0d506fd618aa4f96e6b1ad33
Артём <aroskksk@gmail.com>
2026-07-07 14:47:24 +0300

    Добавил разделы игр

--- script.js
+++ script.js
@@ -89,4 +89,24 @@ ${data.get("comment") || "Нет"}
       }
     });
   }
+ const accordionButtons = document.querySelectorAll(".accordion-btn");
+
+accordionButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.classList.toggle("active");
+
+    const content = button.nextElementSibling;
+
+    if (!content) return;
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+      content.classList.remove("open");
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      content.classList.add("open");
+    }
+  });
+});
+
 });
